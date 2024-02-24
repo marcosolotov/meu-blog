b514a488a5c440b2aa9c6b86581f6ced4e9eeef9
marcosolotov <marcoaureliosolotov@outlook.com>
2024-02-24 19:24:37 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "title":"Inteligência artificial impressiona internautas",
+    "description": "Com o advento das IA's (mais conhecidas como inteligências artificiais), o mundo tem passado por uma mudança drástica, muitos tem medo de que a IA acabe com o seus empregos, dado o impressionante processamento que elas tem.",
+    "photoCover": "https://th.bing.com/th/id/R.0b96cd923bb04cdf6df1adb385fae8de?rik=hJCd8PYGOJIN%2fw&pid=ImgRaw&r=0"
+  },
+  {
+    "id": 2,
+    "title":"Homem-aranha 2 é anunciado",
+    "description": "Com o advento das IA's (mais conhecidas como inteligências artificiais), o mundo tem passado por uma mudança drástica, muitos tem medo de que a IA acabe com o seus empregos, dado o impressionante processamento que elas tem.",
+    "photoCover": "https://www.hdwallpapers.in/download/spider_man_4k_17-3840x2160.jpg"
+  }
+]
